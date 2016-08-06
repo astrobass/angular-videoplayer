df5785c858220913d8c6a5e19cbc8a77dbdc91fc
C W <github@wtree.com>
2016-08-06 19:18:18 -0400

    Initial commit

---
+++ angular-videoplayer.js
@@ -0,0 +1,32 @@
+angular.module('videoplayer', [])
+  .directive('videoplayer', function() {
+    return {
+      name: 'videoplayer',
+      scope: {},
+      controller: 'videoCtrl',
+      restrict: 'E',
+      template: '<video autoplay="true" controls width="200" height="200"></video>',
+      replace: true
+    };
+  })
+  .controller('videoCtrl', function() {
+    var errorCallback = function(e) {
+      console.log('Reeeejected!', e);
+    };
+
+    navigator.getUserMedia  = navigator.getUserMedia ||
+                              navigator.webkitGetUserMedia ||
+                              navigator.mozGetUserMedia ||
+                              navigator.msGetUserMedia;
+
+    navigator.getUserMedia({video: true, audio: true}, function(localMediaStream) {
+      var video = document.querySelector('video');
+      video.src = window.URL.createObjectURL(localMediaStream);
+
+      // Note: onloadedmetadata doesn't fire in Chrome when using it with getUserMedia.
+      // See crbug.com/110938.
+      video.onloadedmetadata = function(e) {
+        // Ready to go. Do some stuff.
+      };
+    }, errorCallback);
+  });
