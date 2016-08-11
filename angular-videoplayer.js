42a3eee78476a5d52a02361c2f9d5f342e6ea7e5
codeanywhere <codeanywhere@wtree.com>
2016-08-11 19:59:40 -0400

    Added canvas to style video

--- angular-videoplayer.js
+++ angular-videoplayer.js
@@ -5,28 +5,27 @@ angular.module('videoplayer', [])
       scope: {},
       controller: 'videoCtrl',
       restrict: 'E',
-      template: '<video autoplay="true" width="300" height="300"></video>',
+      template: '<video id="videoplayer" autoplay></video><canvas id="canvas-video-player"></canvas>',
       replace: true
     };
   })
   .controller('videoCtrl', function() {
+    var successCallback =  function(localMediaStream) {
+      var video = document.getElementById('videoplayer');
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.style = '{ position: absolute; top: 50%; left: 50%; margin: -180px 0 0 -240px; }';
+      var canvas = document.getElementById('canvas-video-player');
+      canvas.style = '{ position: absolute; top: 0; bottom: 0; left: 0; right: 0; width: 100%; height: 100%; }';
+    };
+  
     var errorCallback = function(e) {
       console.log('Reeeejected!', e);
     };
-
+  
     navigator.getUserMedia  = navigator.getUserMedia ||
                               navigator.webkitGetUserMedia ||
                               navigator.mozGetUserMedia ||
                               navigator.msGetUserMedia;
 
-    navigator.getUserMedia({video: true, audio: true}, function(localMediaStream) {
-      var video = document.querySelector('video');
-      video.src = window.URL.createObjectURL(localMediaStream);
-
-      // Note: onloadedmetadata doesn't fire in Chrome when using it with getUserMedia.
-      // See crbug.com/110938.
-      video.onloadedmetadata = function(e) {
-        // Ready to go. Do some stuff.
-      };
-    }, errorCallback);
+    navigator.getUserMedia({video: true, audio: true}, successCallback, errorCallback);
   });
