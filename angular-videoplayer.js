f555d867bd6647c12bc6bd1267b62f17ea4c532f
codeanywhere <codeanywhere@wtree.com>
2016-08-11 20:18:07 -0400

    Added canvas style changes

--- angular-videoplayer.js
+++ angular-videoplayer.js
@@ -5,13 +5,13 @@ angular.module('videoplayer', [])
       scope: {},
       controller: 'videoCtrl',
       restrict: 'E',
-      template: '<video id="videoplayer" autoplay></video><canvas id="canvas-video-player"></canvas>',
+      template: '<video id="video-player" autoplay></video><canvas id="canvas-video-player"></canvas>',
       replace: true
     };
   })
   .controller('videoCtrl', function() {
     var successCallback =  function(localMediaStream) {
-      var video = document.getElementById('videoplayer');
+      var video = document.getElementById('video-player');
       video.src = window.URL.createObjectURL(localMediaStream);
       video.style = '{ position: absolute; top: 50%; left: 50%; margin: -180px 0 0 -240px; }';
       var canvas = document.getElementById('canvas-video-player');
@@ -27,5 +27,25 @@ angular.module('videoplayer', [])
                               navigator.mozGetUserMedia ||
                               navigator.msGetUserMedia;
 
-    navigator.getUserMedia({video: true, audio: true}, successCallback, errorCallback);
+    var constraints = { video: true, audio: true };
+    navigator.getUserMedia(constraints, successCallback, errorCallback);
+  
+//    document.addEventListener('DOMContentLoaded', function(){
+      var v = document.getElementById('video-player');
+      var canvas = document.getElementById('canvas-video-player');
+      var context = canvas.getContext('2d');
+      var cw = Math.floor(canvas.clientWidth / 100);
+      var ch = Math.floor(canvas.clientHeight / 100);
+      canvas.width = cw;
+      canvas.height = ch;
+      v.addEventListener('play', function(){
+          draw(this,context,cw,ch);
+      },false);
+//    },false);
+
+    function draw(v,c,w,h) {
+      if(v.paused || v.ended) return false;
+      c.drawImage(v,0,0,w,h);
+      setTimeout(draw,20,v,c,w,h);
+    }
   });
